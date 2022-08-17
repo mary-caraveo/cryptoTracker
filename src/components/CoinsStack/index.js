64a4c7c134f76e905dfc04f93944b5115469b59f
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-08-17 18:42:25 -0500

    chore: move coin file to the screens folder

--- src/components/CoinsStack/index.js
+++ src/components/CoinsStack/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import CoinsScreen from '../CoinsScreen';
+import CoinsScreen from '../../screens/CoinsScreen';
 import CoinDetailScreen from '../../screens/CoinDetailScreen';
 
 const Stack = createStackNavigator();
