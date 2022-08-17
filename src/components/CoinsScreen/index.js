6a804e4960ed4f90228c3588b2b83b36ff721fb0
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-08-17 18:31:45 -0500

    feat: add navigation between screens

--- src/components/CoinsScreen/index.js
+++ src/components/CoinsScreen/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
 const CoinsScreen = () => {
+  const navigation = useNavigation();
+
+  const handlePress = () => {
+    navigation.navigate('CoinDetail');
+  };
+
   return (
-    <View>
-      <Text>Coins Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Coins Screen</Text>
+      <Pressable style={styles.btn} onPress={handlePress}>
+        <Text style={styles.btnText}>Ir a details</Text>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  titleText: {
+    textAlign: 'center',
+  },
+  btn: {
+    padding: 8,
+    backgroundColor: '#80558C',
+    borderRadius: 8,
+    margin: 16,
+  },
+  btnText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
 
 export default CoinsScreen;
---
+++ src/screens/CoinDetailScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const CoinDetailScreen = () => {
+  return (
+    <View>
+      <Text>Coins Details Screen</Text>
+    </View>
+  );
+};
+
+export default CoinDetailScreen;
