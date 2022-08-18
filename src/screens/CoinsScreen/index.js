ac924d30ba689826158810bf1317367412c042b1
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-08-17 19:16:11 -0500

    feat: add a REST API with fetch

--- src/screens/CoinsScreen/index.js
+++ src/screens/CoinsScreen/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import {View, Text, Pressable, StyleSheet} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import Http from '../../libs/http';
 
 const CoinsScreen = () => {
   const navigation = useNavigation();
+
+  const componentDidMount = async () => {
+    const coins = await Http.instance.get(
+      ' https://api.coinlore.net/api/tickers/ ',
+
+      console.log('coins', coins),
+    );
+  };
 
   const handlePress = () => {
     navigation.navigate('CoinDetail');
