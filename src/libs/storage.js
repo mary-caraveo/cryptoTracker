5a323049a9fc83626496bb8c1b36e0b58a5a28ef
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-08-25 17:52:11 -0500

    feat: create storage library

---
+++ src/libs/storage.js
@@ -0,0 +1,55 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Storage = {store, get, multiGet, getAllKeys, remove};
+
+const store = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(key, value);
+    return true;
+  } catch (err) {
+    console.log('Error Storage store', err);
+    return false;
+  }
+};
+
+const get = async key => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    return value;
+  } catch (err) {
+    console.log('Error Storage get', err);
+    throw Error(err);
+  }
+};
+
+const multiGet = async keys => {
+  try {
+    const values = await AsyncStorage.multiGet(keys);
+    return values;
+  } catch (err) {
+    console.log('Error Storage multiGet', err);
+    throw Error(err);
+  }
+};
+
+const getAllKeys = async () => {
+  try {
+    const keys = await AsyncStorage.getAllKeys();
+    return keys;
+  } catch (err) {
+    console.log('Error Storage getAllKeys', err);
+    throw Error(err);
+  }
+};
+
+const remove = async key => {
+  try {
+    await AsyncStorage.removeItem(key);
+    return true;
+  } catch (err) {
+    console.log('Error Storage remove', err);
+    return false;
+  }
+};
+
+export default Storage;
