49aaa6ffdc58e7b254fe849d801767e12062a3e5
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-08-26 20:32:42 -0500

    feat: add favorites list

--- src/screens/FavoritesScreen/index.js
+++ src/screens/FavoritesScreen/index.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, FlatList, StyleSheet} from 'react-native';
+import {useIsFocused, useNavigation} from '@react-navigation/native';
 import FavoritesEmptyState from '../../components/FavoritesEmptyState';
+import CoinsItem from '../../components/CoinsItem';
 import Colors from '../../resource/colors';
+import Storage from '../../libs/storage';
+
+const getFavorites = async () => {
+  try {
+    const allKeys = await Storage.getAllKeys();
+    const keys = allKeys.filter(key => key.includes('favorite-'));
+    const favs = await Storage.multiGet(keys);
+    return favs.map(fav => JSON.parse(fav[1]));
+  } catch (err) {
+    console.log('Error get favorites', err);
+    return [];
+  }
+};
 
 const FavoritesScreen = () => {
+  const [favorites, setFavorites] = useState([]);
+  const focused = useIsFocused();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (focused) {
+      getFavorites().then(res => setFavorites(res));
+    }
+    return () => {
+      console.log('focused', focused);
+    };
+  }, [focused]);
+
+  const handlePress = coin => {
+    navigation.navigate('CoinDetail', {coin});
+  };
+
   return (
     <View style={styles.container}>
-      <FavoritesEmptyState />
+      {favorites.length === 0 && <FavoritesEmptyState />}
+      {favorites.length > 0 && (
+        <FlatList
+          data={favorites}
+          renderItem={({item}) => (
+            <CoinsItem item={item} onPress={() => handlePress(item)} />
+          )}
+        />
+      )}
     </View>
   );
 };
