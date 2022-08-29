de24181e853c073530389a79354e2d73f69abee0
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-08-29 17:56:44 -0500

    feat: add styles to the empty status page of favorites with nativebase

--- src/components/FavoritesEmptyState/index.js
+++ src/components/FavoritesEmptyState/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'native-base';
 
 const FavoritesEmptyState = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>You don't have any favorite yet</Text>
+    <View flex={1} alignContent="center" justifyContent="center">
+      <Text color="white" fontWeight="bold" fontSize="18" alignSelf="center">
+        You don't have any favorite yet ❤️
+      </Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignContent: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 18,
-    alignSelf: 'center',
-  },
-});
 
 export default FavoritesEmptyState;
